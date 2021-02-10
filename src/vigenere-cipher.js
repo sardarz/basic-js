b50bd92261c891ed0ba86d11d6c2657b98cece66
sardarz <muhkween@gmail.com>
2021-02-10 16:26:09 +0300

    add vigenere cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type = true) {
+    this.alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    this.reversed = !type;
+  }
+
+  encrypt(str, key) {
+    if (!str || !key) throw new Error();
+    str = str.toUpperCase();
+    key = key.toUpperCase();
+    let shifted = [];
+    let counter = 0;
+    let strCounter = 0;
+    while (shifted.length < str.length) {
+      if (this.alph.includes(str[strCounter])) {
+        let shiftBy = this.alph.indexOf(key[counter++]);
+        let idx = this.alph.indexOf(str[strCounter++])
+        shifted.push(this.alph[idx + shiftBy])
+        if (counter >= key.length) counter = 0;
+      } else {
+        shifted.push(str[strCounter++])
+      }
+    }
+    return this.reversed ? shifted.slice(0).reverse().join`` : shifted.join``;
+  }
+
+  decrypt(str, key) {
+    if (!str || !key) throw new Error();
+    str = str.toUpperCase();
+    key = key.toUpperCase();
+    let shifted = [];
+    let counter = 0;
+    let strCounter = 0;
+    while (shifted.length < str.length) {
+      if (this.alph.includes(str[strCounter])) {
+        let shiftBy = this.alph.indexOf(key[counter++]);
+        let idx = this.alph.lastIndexOf(str[strCounter++])
+        shifted.push(this.alph[idx - shiftBy])
+        if (counter >= key.length) counter = 0;
+      } else {
+        shifted.push(str[strCounter++])
+      }
+    }
+    return this.reversed ? shifted.slice(0).reverse().join`` : shifted.join``;
   }
 }
 
